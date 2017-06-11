d2864d2bf92e4730ae799e4944f91e38be376b18
Arijit Bhattacharya <hoodwink73@live.com>
2017-06-11 12:26:23 +0530

    Prettify Gist config

--- src/stories/configs/Gists/index.js
+++ src/stories/configs/Gists/index.js
@@ -4,17 +4,17 @@ import Spinner from 'react-spinkit'
 import Gist from '../../components/Gist'
 import GistIcon from 'react-icons/lib/go/gist'
 import StopIcon from 'react-icons/lib/fa/circle'
-import ErrorIcon from 'react-icons/lib/fa/exclamation-circle';
+import ErrorIcon from 'react-icons/lib/fa/exclamation-circle'
 import parseLinkHeader from 'parse-link-header'
 import { isNil } from 'lodash'
 
 const API_ENDPOINT = function(user) {
-	return `https://api.github.com/users/${user}/gists`
+  return `https://api.github.com/users/${user}/gists`
 }
 
 export default function getFeedConfig(user) {
   var name = `${user}Gists`
-	return {
+  return {
     name,
     itemComponent: Gist,
     itemIdKey: 'id',
@@ -25,51 +25,50 @@ export default function getFeedConfig(user) {
     loaderComponent: function() {
       return <Spinner spinnerName="pulse" fadeIn={0} />
     },
-    errorComponent () {
+    errorComponent() {
       return (
-        <span style={{color: 'red'}}>
-          <ErrorIcon color='red'/> {' Loading Feed'}
+        <span style={{ color: 'red' }}>
+          <ErrorIcon color="red" /> {' Loading Feed'}
         </span>
-
       )
     },
-    noItemsComponent () {
+    noItemsComponent() {
       return (
         <span>
-          <StopIcon color='gray'/>
+          <StopIcon color="gray" />
         </span>
       )
     },
-		getInitialEndpoint(getState) {
-			return API_ENDPOINT(user)
-		},
-		updateEndpoint({headers}) {
+    getInitialEndpoint(getState) {
+      return API_ENDPOINT(user)
+    },
+    updateEndpoint({ headers }) {
       var details = parseLinkHeader(headers.link)
 
-			if (isNil(details) || !details.next) {
-				return ''
-			}
+      if (isNil(details) || !details.next) {
+        return ''
+      }
 
-			return details.next.url
-		},
-		hasMoreItems({headers, results}) {
-			if (isNil(results)) {
-				return false
-			}
+      return details.next.url
+    },
+    hasMoreItems({ headers, results }) {
+      if (isNil(results)) {
+        return false
+      }
 
-			var moreItems
       var details = parseLinkHeader(headers.link)
+      var moreItems
 
-			// does not have details
-			if (isNil(details) || !details.last) {
-				moreItems = false
-			} else if (results.url === details.last.url) {
-				moreItems = false
-			} else if (results.url !== details.last.url) {
-				moreItems = true
-			}
+      // does not have details
+      if (isNil(details) || !details.last) {
+        moreItems = false
+      } else if (results.url === details.last.url) {
+        moreItems = false
+      } else if (results.url !== details.last.url) {
+        moreItems = true
+      }
 
-			return moreItems
-		}
-	}
+      return moreItems
+    }
+  }
 }
